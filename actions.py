f5bc2f7a10226ac93f084ac98fbac02af15d58b9
Bot57 <thibauthubert@msn.com>
2021-07-05 21:44:10 +0200

    1er commit après reprise de la branch "extern"

--- actions.py
+++ actions.py
@@ -1,8 +1,14 @@
-from composants import *
+from typing import List, Any
 
+from composants import *
+from composants import PileJeton, Pioche
+
+global joueurs, pioche_n1, pioche_n2, pioche_n3, pioches, nb_joueur
+global rouge, vert, bleu, noir, blanc, jaune, jetons
 pile_nobles = []
 pioche_n1, pioche_n2, pioche_n3 = Pioche(1), Pioche(2), Pioche(3)
 global joueurs = []
+
 
 
 # Je comprend pas pourquoi je ne peux pas inclures ces variables dans initialisation()
@@ -11,29 +17,28 @@ global joueurs = []
 def initialisation():
 	"""
 	- Définition du nombre de joueur
-    - Définition des joueurs
+	- Définition des joueurs
 	- Mélange et distribution des nobles
 	- Mise en place des jetons
 	- Affichage du jeu en place
 	"""
-	global pioche_n1, pioche_n2, pioche_n3, pioches
-	global rouge, vert, bleu, noir, blanc, jaune, jetons
-	while  nb_joueur >< [2, 3, 4]:
-        nb_joueur = int(input("Quel est le nombre de joueur ? 2, 3 ou 4 ?"))
-        if nb_joueur >< [2, 3, 4]:
-        print("Le nombre de joueur indiqué ne correspond pas.")
-    
-    joueur1 = Joueur(input("Quel est le pseudo du joueur 1 ?")
-    joueurs.extend(joueur1)
-    joueur2 = Joueur(input("Quel est le pseudo du joueur 2 ?")
-    joueurs.extend(joueur2)
-    if nb_joueur >= 3:
-        joueur3 = Joueur(input("Quel est le pseudo du joueur 3 ?")
-        joueurs.extend(joueur3)
-    if nb_joueur == 4:
-        joueur4 = Joueur(input("Quel est le pseudo du joueur 4 ?")
-        joueurs.extend(joueur4)
-    
+	pioche_n1, pioche_n2, pioche_n3 = Pioche(1), Pioche(2), Pioche(3)
+	while nb_joueur != [2, 3, 4]:
+		nb_joueur: int = int(input("Quel est le nombre de joueur ? 2, 3 ou 4 ?"))
+		if nb_joueur != [2, 3, 4]:
+			print("Le nombre de joueur indiqué ne correspond pas.")
+
+	joueur1 = Joueur(input("Quel est le pseudo du joueur 1 ?"))
+	joueurs.extend([joueur1])
+	joueur2 = Joueur(input("Quel est le pseudo du joueur 2 ?"))
+	joueurs.extend([joueur2])
+	if nb_joueur >= 3:
+		joueur3 = Joueur(input("Quel est le pseudo du joueur 3 ?"))
+		joueurs.extend([joueur3])
+	if nb_joueur == 4:
+		joueur4 = Joueur(input("Quel est le pseudo du joueur 4 ?"))
+		joueurs.extend([joueur4])
+
 	init_nobles(nb_joueur)
 	rouge, vert, bleu, noir, blanc, jaune = PileJeton("Rouge", nb_joueur), PileJeton("Vert", nb_joueur), PileJeton(
 		"Bleu", nb_joueur), PileJeton("Noir", nb_joueur), PileJeton("Blanc", nb_joueur), PileJeton("Jaune", nb_joueur),
@@ -50,20 +55,20 @@ def init_nobles(nj):
 	:param nj: nombre de joueurs
 	"""
 	nobles = [(3, 0, 0, 0, 4, 4),
-	          (3, 0, 3, 0, 3, 3),
-	          (3, 3, 3, 0, 3, 0),
-	          (3, 0, 3, 3, 0, 3),
-	          (3, 0, 0, 3, 3, 3),
-	          (3, 4, 4, 0, 0, 0),
-	          (3, 0, 0, 4, 0, 4),
-	          (3, 4, 0, 0, 4, 0),
-	          (3, 0, 4, 4, 0, 0),
-	          (3, 3, 3, 3, 0, 0)]
+			  (3, 0, 3, 0, 3, 3),
+			  (3, 3, 3, 0, 3, 0),
+			  (3, 0, 3, 3, 0, 3),
+			  (3, 0, 0, 3, 3, 3),
+			  (3, 4, 4, 0, 0, 0),
+			  (3, 0, 0, 4, 0, 4),
+			  (3, 4, 0, 0, 4, 0),
+			  (3, 0, 4, 4, 0, 0),
+			  (3, 3, 3, 3, 0, 0)]
 
 	shuffle(nobles)
 	global pile_nobles
 	pile_nobles = [Noble(nobles[i][0], nobles[i][1], nobles[i][2], nobles[i][3], nobles[i][4], nobles[i][5]) for i in
-	               range(nj + 1)]
+				   range(nj + 1)]
 
 
 def afficher_jeu(p1=pioche_n1, p2=pioche_n2, p3=pioche_n3):
@@ -117,13 +122,13 @@ def pioche3jetons(joueur):
 		while compteur == 0:
 			couleur: str = input(f"""Quel jeton voulez vous en {tour}? (indiquez "annuler" pour annuler)
 			Jeton disponibles:
-            - rouge: {rouge.nb_jetons}
-            - vert: {vert.nb_jetons} 
-            - bleu: {bleu.nb_jetons}
-            - noir: {noir.nb_jetons}
-            - blanc: {blanc.nb_jetons}"""
-            if double_couleur == "annuler":
-                return False # utilisé pour indiquer que l'action n'est pas validé et reproposer les acions possible
+			- rouge: {rouge.nb_jetons}
+			- vert: {vert.nb_jetons} 
+			- bleu: {bleu.nb_jetons}
+			- noir: {noir.nb_jetons}
+			- blanc: {blanc.nb_jetons}""")
+			if couleur == "annuler":
+				return False # utilisé pour indiquer que l'action n'est pas validé et reproposer les acions possible
 			elif couleur not in jetons:
 				print("Votre saisie est incorrect !\nVeuillez indiquez l'une des couleurs suivantes:")
 				continue
@@ -140,53 +145,53 @@ def pioche3jetons(joueur):
 		return couleur
 
 	couleur1 = piocherjeton(joueur, "premier")
-    if couleur1 == False:
-        return False
+	if couleur1 == False:
+		return False
 	couleur2 = piocherjeton(joueur, "second", [couleur1])
-    if couleur2 == False:
-        return False
+	if couleur2 == False:
+		return False
 	couleur3 = piocherjeton(joueur, "troisième", [couleur1, couleur2])
-    if couleur3 == False:
-        return False
+	if couleur3 == False:
+		return False
 
 	print(f"Vous avez choisi un jeton {couleur1}, {couleur2} et {couleur3}."
-	      f"Vous avez maintenant {joueur.jetons['rouge']} rouge(s), {joueur.jetons['vert']} vert(s), "
-	      f"{joueur.jetons['bleu']} bleu(s), {joueur.jetons['noir']} noir(s) et {joueur.jetons['blanc']} blanc(s)")
-    return True
-    
+		  f"Vous avez maintenant {joueur.jetons['rouge']} rouge(s), {joueur.jetons['vert']} vert(s), "
+		  f"{joueur.jetons['bleu']} bleu(s), {joueur.jetons['noir']} noir(s) et {joueur.jetons['blanc']} blanc(s)")
+	return True
+
 
 def pioche2jetons(joueur, compteur=0):
 	"""
 	Pioche de 2 jetons de même couleurs
 	:param compteur: Permet de vérifier si l'action est fini
 	:param joueur: Joueur qui pioche les jetons
-    :return: True si action terminé, False si action non aboutie
+	:return: True si action terminé, False si action non aboutie
 	"""
 	while compteur == 0:
 		double_couleur: str = input(f"""Quel jeton voulez vous en double? (indiquez "annuler" pour annuler)
 		Jeton disponibles:
-        - rouge: {rouge.nb_jetons}
-        - vert: {vert.nb_jetons} 
-        - bleu: {bleu.nb_jetons}
-        - noir: {noir.nb_jetons}
-        - blanc: {blanc.nb_jetons}""")
-        
+		- rouge: {rouge.nb_jetons}
+		- vert: {vert.nb_jetons} 
+		- bleu: {bleu.nb_jetons}
+		- noir: {noir.nb_jetons}
+		- blanc: {blanc.nb_jetons}""")
+
 		if double_couleur == "annuler":
-            return False # utilisé pour indiquer que l'action n'est pas validé et reproposer les acions possible
-        elif double_couleur not in jetons:
+			return False # utilisé pour indiquer que l'action n'est pas validé et reproposer les acions possible
+		elif double_couleur not in jetons:
 			print("Votre saisie est incorrect !\nVeuillez indiquez l'une des couleurs suivantes:")
 			continue
 		elif jetons[double_couleur].nb_jetons < 4:
 			print("Cette action n'est pas possible si il y a moins de 4 jetons de cette couleur disponibles\n"
-			      "Veuillez choisir une couleur disponible :")
+				  "Veuillez choisir une couleur disponible :")
 			continue
 		else:
 			joueur.jetons[double_couleur] += 2
 			jetons[double_couleur].nb_jetons -= 2
 			compteur += 1
 	print(f"Vous avez choisi 2 jetons {double_couleur}."
-	      f"Vous avez maintenant {joueur.jetons['rouge']} rouge(s), {joueur.jetons['vert']} vert(s), "
-	      f"{joueur.jetons['bleu']} bleu(s), {joueur.jetons['noir']} noir(s) et {joueur.jetons['blanc']} blanc(s)")
+		  f"Vous avez maintenant {joueur.jetons['rouge']} rouge(s), {joueur.jetons['vert']} vert(s), "
+		  f"{joueur.jetons['bleu']} bleu(s), {joueur.jetons['noir']} noir(s) et {joueur.jetons['blanc']} blanc(s)")
 	return True
 
 
@@ -206,10 +211,10 @@ def choisir_carte(joueur):
 		:return: True si le joueur a assez de jeton, sinon False
 		"""
 		return (joueurbis.jetons["rouge"] + joueurbis.bonus["rouge"]) >= carte.rouge and \
-		       (joueurbis.jetons["vert"] + joueurbis.bonus["vert"]) >= carte.vert and \
-		       (joueurbis.jetons["bleu"] + joueurbis.bonus["bleu"]) >= carte.bleu and \
-		       (joueurbis.jetons["noir"] + joueurbis.bonus["noir"]) >= carte.noir and \
-		       (joueurbis.jetons["blanc"] + joueurbis.bonus["blanc"]) >= carte.blanc
+			   (joueurbis.jetons["vert"] + joueurbis.bonus["vert"]) >= carte.vert and \
+			   (joueurbis.jetons["bleu"] + joueurbis.bonus["bleu"]) >= carte.bleu and \
+			   (joueurbis.jetons["noir"] + joueurbis.bonus["noir"]) >= carte.noir and \
+			   (joueurbis.jetons["blanc"] + joueurbis.bonus["blanc"]) >= carte.blanc
 
 	pioche = pioches[input("De quel niveau est la carte que vous souhaitez ? (1, 2 ou 3?)")]
 	numero = int(input("Quel est le numéro de la carte que vous souhaitez? (1, 2, 3 ou 4?)"))
@@ -217,18 +222,18 @@ def choisir_carte(joueur):
 	if joueur.jetons["jaune"] > 0:
 		utiliseJaune = input("Voulez vous utiliser un jeton jaune ? (O/N)")
 		if utiliseJaune == "O":
-			while check 0 :
-                couleurChoisi = input("Quelle est la couleur que vous voulez que votre jeton jaune représente ? \n"
-                                    "rouge, vert, bleu, noir ou blanc ?")
-                if couleurChoisi not in jetons:
-                    print("Votre saisie est incorrect !")
-                    continue
-                else : 
-                    print(f"Vous avez choisi la couleur {couleurChoisi}")
-                    joueur.jetons[couleurChoisi] += 1
-                    check = 1
-    else:
-        utiliseJaune = "N"
+			while check == 0:
+				couleurChoisi = input("Quelle est la couleur que vous voulez que votre jeton jaune représente ? \n"
+									"rouge, vert, bleu, noir ou blanc ?")
+				if couleurChoisi not in jetons:
+					print("Votre saisie est incorrect !")
+					continue
+				else:
+					print(f"Vous avez choisi la couleur {couleurChoisi}")
+					joueur.jetons[couleurChoisi] += 1
+					check = 1
+	else:
+		utiliseJaune = "N"
 
 	if check_jetons(joueur, carteChoisie):
 		joueur.achete_carte(pioche, numero)
